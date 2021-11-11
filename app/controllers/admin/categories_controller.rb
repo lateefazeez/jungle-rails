34d2c8c66ae1cb57d7107090e3008a684f1dd5b9
Lateef Azeez <codelate18@gmail.com>
2021-11-10 18:40:20 -0700

    build the admin dashboard to show categories and add new

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -9,4 +9,26 @@ class Admin::CategoriesController < ApplicationController
     @products = @category.products.order(created_at: :desc)
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create 
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin_categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
 end
